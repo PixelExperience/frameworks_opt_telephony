861e812e96fa37ca4f78a71b1555385d9a66f675
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2018-07-20 15:07:01 +0530

    CDMA MO SMS follow on DC feature
    Add new API to send MO CDMA SMS with expectMore option.
    
    CRs-Fixed: 2242430
    Change-Id: I5f504628186eab50f18584251dcbe198483b360f

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -259,7 +259,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
     private WorkSource mActiveWakelockWorkSource;
 
     /** Telephony metrics instance for logging metrics event */
-    private TelephonyMetrics mMetrics = TelephonyMetrics.getInstance();
+    protected TelephonyMetrics mMetrics = TelephonyMetrics.getInstance();
     /** Radio bug detector instance */
     private RadioBugDetector mRadioBugDetector = null;
 
@@ -3483,7 +3483,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
         }
     }
 
-    private void constructCdmaSendSmsRilRequest(CdmaSmsMessage msg, byte[] pdu) {
+    protected void constructCdmaSendSmsRilRequest(CdmaSmsMessage msg, byte[] pdu) {
         int addrNbrOfDigits;
         int subaddrNbrOfDigits;
         int bearerDataLength;
